098fc61b73e0a430b3f6291b636e74294bc06c66
Adam Oliver Zsigmond <6388483+zsedem@users.noreply.github.com>
2021-01-01 10:48:15 +0100

    make some real smoke test to the python binding

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -19,29 +19,25 @@
 module Main
   ( main ) where
 
-import           Control.Monad.IO.Class (liftIO)
-
 import qualified CPython as Py
 import qualified CPython.Types.Module as Py
 import qualified CPython.Protocols.Object as Py
-import qualified CPython.Types.Capsule as PyCapsule
+import qualified CPython.Protocols.Number as Py
 import qualified CPython.Types.Dictionary as PyDict
+import qualified CPython.Types.List as PyList
 import qualified CPython.Types.Tuple as PyTuple
 import qualified CPython.Types.Unicode as PyUnicode
+import qualified CPython.Types.Integer as PyInt
 import qualified CPython.Types.Exception as PyExc
-import Data.Text hiding(take)
-import System.Exit(exitSuccess)
-import Foreign.StablePtr
-import Foreign.Ptr
-import Foreign.C.String
+import Data.Text()
+import Control.Monad(guard)
+import Data.Maybe(fromMaybe)
 import Control.Exception(handle, SomeException)
 
 main :: IO ()
 main = verboseExc $ handle pyExceptionHandler $ do
   Py.initialize
-  newHome <- liftIO Py.getPythonHome
-  print newHome
-  _ <- liftIO (Py.importModule "os")
+  testingSomePythonEvaluation
   Py.finalize
   where
     pyExceptionHandler :: PyExc.Exception -> IO ()
@@ -63,3 +59,17 @@ main = verboseExc $ handle pyExceptionHandler $ do
 
     handleEverything :: (SomeException -> IO a) -> IO a -> IO a
     handleEverything = handle
+
+testingSomePythonEvaluation :: IO ()
+testingSomePythonEvaluation = do
+    testList <- traverse toObj [1, 10, 100, 42] >>= PyList.toList >>= (return . Py.toObject)
+    builtinsModule <- Py.importModule "builtins"
+    sumFunc <- PyUnicode.toUnicode "sum" >>= Py.getAttribute builtinsModule
+    args <- PyTuple.toTuple [testList]
+    kwargs <- PyDict.new
+    result <- Py.call sumFunc args kwargs >>= castToNumber >>= Py.toInteger >>= PyInt.fromInteger
+    guard $ result == 153
+  where
+    castToNumber obj = do x <- Py.castToNumber obj
+                          return $ fromMaybe (error "not a number returned from the sum") x
+    toObj integer = fmap Py.toObject $ PyInt.toInteger integer
